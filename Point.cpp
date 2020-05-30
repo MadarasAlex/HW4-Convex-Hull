811c3af359dff3f423caf8bcab696179c01448cd
Alex Madaras <62357167+MadarasAlex@users.noreply.github.com>
2020-05-30 13:11:44 +0300

    Create Point.cpp

---
+++ Point.cpp
@@ -0,0 +1,33 @@
+
+#include "Point.h"
+Point::Point(int x, int y)
+    :x(x),y(y){
+
+}
+
+Point::Point(const Point &point2)
+    :x(point2.x), y(point2.y){
+
+}
+
+int Point::getX() const{
+    return this->x;
+}
+
+void Point::setX(int inputX) {
+    this->x = inputX;
+}
+
+int Point::getY() const{
+    return this->y;
+}
+
+void Point::setY(int inputY) {
+    this->y = inputY;
+}
+
+Point::Point()
+    :x(0),y(0) {
+
+}
+
